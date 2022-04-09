827d12d06fa15b80e8bb7bdd893ba3be138223fa
Steven <hosteve334@gmail.com>
2022-04-08 20:41:56 -0400

    find best model

--- script.py
+++ script.py
@@ -21,25 +21,30 @@ predict = "G3" #also known as a label
 #Training data to predict another value
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
 
 #taking attributes and splitting them into four different arrays 
 #the test are to test the accuracy of our model
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
 
-"""
-#!!!Uncomment to create a new linear model that is saved to studentmodel.pickle!!!
+best = 0
+for _ in range(30):
 
-linear = linear_model.LinearRegression()
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
 
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test,y_test)
-#Acutal model that predicts scores at the end of the year
-print(accuracy)
+    linear = linear_model.LinearRegression()
 
-#Storing model
-with open('studentmodel.pickle','wb') as f:
-    pickle.dump(linear, f)
-"""
+    linear.fit(x_train, y_train)
+    accuracy = linear.score(x_test,y_test)
+    #Acutal model that predicts scores at the end of the year
+    print(accuracy)
+
+    #finding the best model
+    if accuracy > best:
+        best = accuracy
+        #Storing model
+        with open('studentmodel.pickle','wb') as f:
+            pickle.dump(linear, f)
+
 
 
 #reading model file
