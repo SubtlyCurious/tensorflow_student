ee37153acba4ded369e084c48105f1a06b6ebbe4
Steven <hosteve334@gmail.com>
2022-04-08 20:21:33 -0400

    Finished machine learning linear regression tutorial

--- script.py
+++ script.py
@@ -21,4 +21,22 @@ y = np.array(data[predict])
 
 #taking attributes and splitting them into four different arrays 
 #the test are to test the accuracy of our model
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test,y_test)
+#Acutal model that predicts scores at the end of the year
+print(accuracy)
+
+#print line coefficients
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+
+#Array of Arrays
+predictions = linear.predict(x_test)
+
+#print out all the predictions and display input data 
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
