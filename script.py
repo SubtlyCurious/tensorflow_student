e1cc0071a787da81fae2be67e9271faed349d717
Steven <hosteve334@gmail.com>
2022-04-08 20:37:47 -0400

    save model using pickle

--- script.py
+++ script.py
@@ -3,7 +3,10 @@ import numpy
 import sklearn
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as pyplot
+import pickle
 
+from matplotlib import style
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
@@ -23,12 +26,25 @@ y = np.array(data[predict])
 #the test are to test the accuracy of our model
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
 
+"""
+#!!!Uncomment to create a new linear model that is saved to studentmodel.pickle!!!
+
 linear = linear_model.LinearRegression()
 
 linear.fit(x_train, y_train)
 accuracy = linear.score(x_test,y_test)
 #Acutal model that predicts scores at the end of the year
 print(accuracy)
+
+#Storing model
+with open('studentmodel.pickle','wb') as f:
+    pickle.dump(linear, f)
+"""
+
+
+#reading model file
+pickle_in = open('studentmodel.pickle', 'rb')
+linear = pickle.load(pickle_in)
 
 #print line coefficients
 print('Coefficient: \n', linear.coef_)
@@ -40,3 +56,4 @@ predictions = linear.predict(x_test)
 #print out all the predictions and display input data 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
